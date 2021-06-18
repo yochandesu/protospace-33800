665da8497fa901b517d503a56284f3689a319783
yochandesu <abeyoshiaki0712@yahoo.co.jp>
2021-06-18 20:23:35 +0900

    Update 20210519015841_create_prototypes.rb

--- db/migrate/20210519015841_create_prototypes.rb
+++ db/migrate/20210519015841_create_prototypes.rb
@@ -2,10 +2,10 @@ class CreatePrototypes < ActiveRecord::Migration[6.0]
   def change
     create_table :prototypes do |t|
       t.string :title
-      t.text :catch_copy, foreign_key: true
-      t.text :concept, foreign_key: true
+      t.text :catch_copy
+      t.text :concept
       t.timestamps
-      t.references :user_id, foreign_key: true
+      t.references :user, foreign_key: true
     end
   end
 end
